3270d5f92f13f65980b4f07de76a27d6672b0fff
Karin Hermann <karinclimber@gmail.com>
2018-08-05 19:03:44 -0400

    new pics

--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -129,13 +129,13 @@ $(document).ready(function() {
 
     var qimageArray = new Array(); 
     qimageArray[0] = "<img class='center-block' src='/Users/karinhermann/Desktop/Craft-Beer-Trivia/beer/app/assets/images/juicyjay.png>";
-    qimageArray[1] = "<img class='center-block' src='../assets/images/dryhopping.png'>"; 
-    qimageArray[2] = "<img class='center-block' src='../assets/images/circusboyanswer.png'>"; 
-    qimageArray[3] = "<img class='center-block' src='../assets/images/somethinglikethatbefore-01.png>";  
-    qimageArray[4] = "<img class='center-block' src='../assets/images/gosequestion.png'>"; 
-    qimageArray[5] = "<img class='center-block' src='../assets/images/coffeehop.jpg'>"; 
-    qimageArray[6] = "<img class='center-block' src='../assets/images/kbs.jpg'>"; 
-    qimageArray[7] = "<img class='center-block' src='../assets/images/farmhousewin.jpg'>"; 
+    qimageArray[1] = "<img class='center-block' src='/Users/karinhermann/Desktop/Craft-Beer-Trivia/beer/beerapp/assets/images/dryhopping.png'>"; 
+    qimageArray[2] = "<img class='center-block' src='/Users/karinhermann/Desktop/Craft-Beer-Trivia/beer/beerapp/assets/images/circusboyanswer.png'>"; 
+    qimageArray[3] = "<img class='center-block' src='/Users/karinhermann/Desktop/Craft-Beer-Trivia/beer/beerapp/assets/images/somethinglikethatbefore-01.png>";  
+    qimageArray[4] = "<img class='center-block' src='/Users/karinhermann/Desktop/Craft-Beer-Trivia/beer/beerapp/assets/images/gosequestion.png'>"; 
+    qimageArray[5] = "<img class='center-block' src='/Users/karinhermann/Desktop/Craft-Beer-Trivia/beer/beerapp/assets/images/coffeehop.jpg'>"; 
+    qimageArray[6] = "<img class='center-block' src='/Users/karinhermann/Desktop/Craft-Beer-Trivia/beer/beerapp/assets/images/kbs.jpg'>"; 
+    qimageArray[7] = "<img class='center-block' src='/Users/karinhermann/Desktop/Craft-Beer-Trivia/beer/beerapp/assets/images/farmhousewin.jpg'>"; 
 
     var answerArray = [
         ["Heist", "Legion", "Sycamore", "Bird Song"], 
