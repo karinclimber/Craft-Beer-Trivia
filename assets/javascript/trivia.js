e373fd0e094df7b40237d5ea97350d3da68ed8be
Karin Hermann <karinclimber@gmail.com>
2018-08-05 18:51:47 -0400

    new pics

--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -68,7 +68,7 @@ $(document).ready(function() {
     }
     
     function generateQuestions() {
-        gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>30</span></p><p class='text-center'>" + qimageArray[questionCounter] + questionArray[questionCounter] + "</p><p class='first-answer answer'>A. " + answerArray[questionCounter][0] + "</p><p class='answer'>B. "+answerArray[questionCounter][1]+"</p><p class='answer'>C. "+answerArray[questionCounter][2]+"</p><p class='answer'>D. "+answerArray[questionCounter][3]+"</p>";
+        gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>30</span></p>" + qimageArray[questionCounter] + questionArray[questionCounter] + "<p class='first-answer answer'>A. " + answerArray[questionCounter][0] + "</p><p class='answer'>B. "+answerArray[questionCounter][1]+"</p><p class='answer'>C. "+answerArray[questionCounter][2]+"</p><p class='answer'>D. "+answerArray[questionCounter][3]+"</p>";
         $("#mainArea").html(gameHTML);
     }
     
